0cb4015f79541702ac50b74dde9b3c3663f0d818
Julian Smith <juliansmith94@hotmail.com>
2026-05-14 16:54:25 -0500

    add prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
   adapter: netlify(),
   integrations: [react()],
   server: {
